48361d28a90ad15fe1d4fdfbcd08423ebf83ce49
bhermosilloa <62979015+bhermosilloa@users.noreply.github.com>
2022-02-13 11:44:36 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+
 
 export default function Home() {
   return (
@@ -11,10 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="not drinkprime" />
       </main>
 
       <Footer />
